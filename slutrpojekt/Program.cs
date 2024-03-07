d672d54ac11b8bfb99a359807c20dd65ec4cfc01
FelixGitHub2 <felix.elliot@elev.ga.ntig.se>
2024-03-07 14:10:50 +0100

    first commit

---
+++ slutrpojekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+Raylib.SetTargetFPS(60);
+Raylib.InitWindow(1920, 1080, "Slutprojekt");
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.ClearBackground(Color.LightGray);
+
+    Raylib.EndDrawing();
+}
