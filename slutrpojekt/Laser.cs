f2ad5c46fbb02a7d468dee31ada6ec6aed0def75
FelixGitHub2 <felix.elliot@elev.ga.ntig.se>
2024-03-21 13:23:22 +0100

    lol

--- slutrpojekt/Laser.cs
+++ slutrpojekt/Laser.cs
@@ -3,5 +3,16 @@ using System.Numerics;
 
 public class Laser
 {
+    //variables
+    int x;
+    int y;
+    int speed;
 
+    public Laser(int playerPosX, int PlayerPosY)
+    {
+        x = playerPosX;
+        y = PlayerPosY;
+
+        speed = 10;
+    }
 }
